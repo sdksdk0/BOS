3c1bd40dd741053d3f7c5f92df4a24d2ecc1a111
sdksdk0 <690644511@qq.com>
2016-09-12 23:27:55 +0800

    test
    测试包文件

---
+++ src/main/java/cn/tf/bos/domain/User.java
@@ -0,0 +1,121 @@
+package cn.tf.bos.domain;
+
+import java.util.Date;
+
+/**
+ * User entity. @author MyEclipse Persistence Tools
+ */
+
+public class User implements java.io.Serializable {
+
+	// Fields
+
+	private String id;
+	private String username;
+	private String password;
+	private Double salary;
+	private Date birthday;
+	private String gender;
+	private String station;
+	private String telephone;
+	private String remark;
+
+	// Constructors
+
+	/** default constructor */
+	public User() {
+	}
+
+	/** minimal constructor */
+	public User(String id) {
+		this.id = id;
+	}
+
+	/** full constructor */
+	public User(String id, String username, String password, Double salary, Date birthday, String gender, String station, String telephone, String remark) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.salary = salary;
+		this.birthday = birthday;
+		this.gender = gender;
+		this.station = station;
+		this.telephone = telephone;
+		this.remark = remark;
+	}
+
+	// Property accessors
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Double getSalary() {
+		return this.salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Date getBirthday() {
+		return this.birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getGender() {
+		return this.gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getStation() {
+		return this.station;
+	}
+
+	public void setStation(String station) {
+		this.station = station;
+	}
+
+	public String getTelephone() {
+		return this.telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getRemark() {
+		return this.remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+}
